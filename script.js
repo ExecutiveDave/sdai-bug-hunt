aa50e260397594fe6eaae3307feb27173c9d750d
ExecutiveDave <davidwilliams.ca68@icloud.com>
2024-11-19 04:59:48 +0000

    debugging

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-
+// JS Fix #1 Line 1 getElementById requires the keyword document in front
+document.getElementById('funFactButton') // JS Fix #2 Line 2 the ID name is incorrect -  original: funfactButton // funFactButton
+  .addEventListener('click', function() {
+  const funFact = document.getElementById('funFact'); // JS Fix #3 Line 4 the ID name is incorrect - funfact
+  if (funFact.style.display === 'none') {
+      funFact.style.display = 'block';
+  } else {
+      funFact.style.display = 'none';
+  }
+});
