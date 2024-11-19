264d90c4935710586b1fd85214a6985a9a26f94d
ExecutiveDave <davidwilliams.ca68@icloud.com>
2024-11-19 04:47:12 +0000

    fix bugs in html,css,js

--- script.js
+++ script.js
@@ -1,8 +1,1 @@
-getElementById('funfactButton').addEventListener('click', function() {
-  const funFact = document.getElementById('funfact');
-  if (funFact.style.display === 'none') {
-      funFact.style.display = 'block';
-  } else {
-      funFact.style.display = 'none';
-  }
-});
+
